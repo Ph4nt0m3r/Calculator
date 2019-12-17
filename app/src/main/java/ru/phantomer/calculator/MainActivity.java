792d07a0d5b3266fd0c534b462c274f65cebad56
Ph4nt0m3r <58989398+Ph4nt0m3r@users.noreply.github.com>
2019-12-17 21:30:44 +0300

    Add android studio project

---
+++ app/src/main/java/ru/phantomer/calculator/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.phantomer.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+        EditText firstInput = findViewById(R.id.editText);
+        EditText secondInput = findViewById(R.id.editText2);
+        TextView output = findViewById(R.id.textView);
+        output.setText(firstInput.getText().toString() + secondInput.getText().toString());
+
+    }
+
+}
